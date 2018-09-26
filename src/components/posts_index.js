367646b48920bc6522dca2626135c47295713ac1
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2018-09-26 13:17:03 -0700

    first react-blog push

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        postsindex worked
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
