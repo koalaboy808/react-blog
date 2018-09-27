5149e42be5183bb27e86deae2bbe19c651ee0891
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2018-09-27 16:06:06 -0700

    add post index page with action and reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
+    console.log("this is this.props ", this.props);
     return (
       <div>
-        postsindex worked
+        <h3>
+          Posts
+        </h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+// In order to get list of posts inside componenent,
+// we need to consume from application level state.
+// We always define our map state to prop's function
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        post hello
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
